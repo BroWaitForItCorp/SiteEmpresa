3e114ce5a02721252098c704f52fc558dc5538e9
BroCorpInfo <brocorp.contato@gmail.com>
2017-04-16 01:10:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<html>
+
+<head>
+<title>BroCorp Informática </title>
+<link rel="stylesheet" href="css.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:300" rel="stylesheet">
+<link rel="stylesheet" href="aos.css" />
+<script src="aos.js"></script>
+
+</head>
+
+<body>
+<script>
+    AOS.init();
+  </script>
+<div id="menu" style="z-index:10">
+<ul class="snip1217" style="font-family: 'Work Sans', sans-serif; ">
+  <li class="current"><a href="index.php">Inicio</a></li>
+  <li><a href="info.php">Informações</a></li>
+  <li><a href="servicos.php">Serviços</a></li>
+  <li><a href="contato.php">Contato</a></li>
+</ul>
+</div>
+<br>
+<br>
+<div id="header" align="center;">
+<img src="logo.png" width="40.5%" height="120%" style="margin-left: 295;">
+</div>
+<div id="corpo" align="center"><br>
+<!-- Olá, pessoa bonita!-->
+<label style="font-size: 35px;"><label style="color: green;">-</label>  Olá, pessoa bonita.  <label style="color: green;">-</label></label><br><br>
+
+<!-- Sempre sonhou em criar um programa eficiente, bonito e blablabla? !-->
+<label style="font-size: 20px;">Sempre sonhou em criar um programa <b style="color: grey;">eficiente</b>, <b style="color: grey;">bonito</b> 
+e <b style="color: lightblue;">divertido</b>? <br>Precisa criar um <b style="color: lightblue;">WEBSITE</b> pra sua empresa e não sabe o que 
+fazer? <br>Quer realizar o sonho de ter um <b style="color: grey;">JOGO</b> só seu? Então...</label><br><br><br>
+<label style="font-size: 38px;"><b style="color: #ff025b;">VOCÊ</b> veio ao lugar certo :)</label><br><br><br>
+<label style="font-size: 20px;">Nós somos a <b style="color: #ff025b;">BroCorp</b>, uma empresa focada na área da informática.<br>Estamos aqui para tornar seu sonho
+ realidade.</label><br><hr width="50%">
+ 
+ <br>
+ <br>
+<div id="coluna1" data-aos="fade">
+<img src="icons/dev.png" width="70" height="70"><br>
+<label style="font-size: 18px;"><b>DESENVOLVIMENTO</b></label>
+<br>
+<br>
+- Aplicativos para Desktop (Windows).<br>
+- Aplicativos para Android.<br>
+- Websites (básicos e profissionais).<br>
+- Jogos 2D e 3D para Desktop. <br>
+- Jogos 2D para Android. <br>
+- Dentre outros... <br>
+</div>
+
+<div id="coluna2" data-aos="fade">
+<img src="icons/manu.png" width="70" height="70"><br>
+<label style="font-size: 18px;"><b>MANUTENÇÃO</b></label><br><br>
+- Configuração de WI-FI (roteadores).<br>
+- Formatação de computadores e celulares.<br>
+- Instalação de sistemas operacionais.<br>
+- Blindagem de pen-drivers.<br>
+- Limpeza de PC'S. <br>
+- Dentre outros... <br>
+</div>
+ 
+<div id="coluna3" data-aos="fade">
+<img src="icons/bas.png" width="70" height="70"><br>
+<label style="font-size: 18px;"><b>INFORMÁTICA BÁSICA</b></label><br><br>
+- Criação de slides (profissionais ou básicos) para apresentações.<br>
+- Digitação de trabalhos/documentos.<br>
+- Instalação de anti-vírus.<br>
+- Instalação de programas.<br>
+- Dentre outros... <br>
+</div>
+<br>
+<label style="font-size: 20px;">Quer saber mais? Clique no botão maravilhoso abaixo:</label><br><br>
+<button class="btn btn-1 btn-1a" name="logar" width="50%" height="50%" style="">Botão Maravilhoso Abaixo</button>
+</div>
+
+
+<div id="fundo" align="center"><br>
+Uma empresa por:<br>
+<label data-aos="fade-up">Saulo Joab | João Alves | Warley de Lima | Hélber Matias | Isaac Moraes</label><br>
+<a href="index.php" ><img src="brocorp.png" width="10%" data-aos="fade-up"></a><br>
+<label>BroCorp Informática - 2017</label><br><br>
+<label name="logar" width="20%" height="10%" style="margin-top: -10;"></label>
+
+</div>
+
+</body>
+
+</html>
+			 
